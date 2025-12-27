d4693ea1281b24143ba82a2bc0fd9fcad50e84c0
Nagabhushanam Dusa <naveendusa490@gmail.com>
2025-12-27 23:06:22 +0530

    Service

---
+++ AccountService.java
@@ -0,0 +1,26 @@
+package com.example.banking.service;
+
+import java.util.List;
+
+import com.example.banking.dto.AccountDto;
+
+
+public interface AccountService {
+
+	AccountDto createAccount(AccountDto accountDto);
+	
+	
+	AccountDto getAccountById(Long id);
+	
+	AccountDto deposite(Long id,double amount);
+	
+	AccountDto withdraw(Long id,double amount);
+	
+	List<AccountDto> getAllAccounts();
+	
+	void deleteAccount(Long id);
+
+
+	
+
+}
